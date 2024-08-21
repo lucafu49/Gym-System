3f819c5e026de83de829104c31923d93400568db
lucafu49 <lucasmena1403@gmail.com>
2024-08-21 14:28:37 -0300

    Agregar archivos de proyecto.

---
+++ GymAkam/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GymAkam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Obtén la cadena de conexión desde el archivo app.config
+            string connectionString = ConfigurationManager.ConnectionStrings["GymAkam.Properties.Settings.GymAkamConnectionString"].ConnectionString;
+
+            // Usa la cadena de conexión para conectarte a la base de datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Conexión exitosa a la base de datos.");
+
+                    // Aquí puedes ejecutar tus comandos SQL
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al conectar a la base de datos: " + ex.Message);
+                }
+            }
+        }
+    }
+}
